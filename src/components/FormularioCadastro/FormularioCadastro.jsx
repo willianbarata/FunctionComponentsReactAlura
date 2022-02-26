43e13285d9c08324ce24fc04c1d9e0f35e730f62
willianbarata <willianbarata@gmail.com>
2022-02-26 09:17:34 -0300

    Aula 10 - Injeção de dependencia no envio

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
-function FormularioCadastro(){
+function FormularioCadastro({aoEnviar}){
 
     const [nome, setNome] = useState("");
     const [sobrenome, setSobrenome] = useState("");
@@ -12,7 +12,7 @@ function FormularioCadastro(){
     return(
         <form onSubmit={(event)=>{
             event.preventDefault();
-            console.log({nome, sobrenome, cpf, promocoes, novidades});
+            aoEnviar({nome, sobrenome, cpf, promocoes,novidades});
         }}> 
             <TextField  
             value={nome}
